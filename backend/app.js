97dc1f87c81ad4034d698e6c27784143ecbe2298
santt3131 <santt31@gmail.com>
2021-07-27 00:26:21 +0200

    Re-Organizing Backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
+
+const postRoutes = require("./routes/posts");
 const Post = require("./models/post");
 
 mongoose.connect("mongodb+srv://santt31:Alaska123@cluster0.oreks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -30,65 +32,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: 'Post fetched succesfully!',
-      posts: documents
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      this.response.status(404).json({
-        message: "Post not found"
-      })
-    }
-  });
-});
-
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(req.params.id);
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post Deleted!!"
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: "Update successfull"
-    })
-  });
-});
+//importamos
+app.use("/api/posts", postRoutes);
 
 //exportamos la aplicacion
 module.exports = app;
