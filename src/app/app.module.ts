36e41438448b4e8f81f91dace3643069b5d023a5
santt3131 <santt31@gmail.com>
2021-07-27 23:00:35 +0200

    Convert Template Driven to reactive Approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -26,7 +26,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup , FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../../post.model';
 import { PostsService } from '../../posts.service';
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   enteredContent = '';
   post:Post;
   isLoading = false;
+  form: FormGroup;
   private mode="create";
   private postId:string;
 
@@ -24,6 +25,11 @@ export class PostCreateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [ Validators.required, Validators.minLength(3) ] }),
+      content: new FormControl(null, { validators: [ Validators.required ] }),
+    });
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if(paramMap.has('postId')){
         this.mode = 'edit';
@@ -35,7 +41,11 @@ export class PostCreateComponent implements OnInit {
             id: postData._id,
             title:postData.title,
             content: postData.content
-          }
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          });
         });
       }else{
         this.mode = 'create';
@@ -44,17 +54,17 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if(this.mode ==='create'){
-      this.postsService.addPost(form.value.title,form.value.content);
+      this.postsService.addPost(this.form.value.title,this.form.value.content);
     }else{
-      this.postsService.updatePost(this.postId,form.value.title,form.value.content );
+      this.postsService.updatePost(this.postId,this.form.value.title,this.form.value.content );
     }
-    form.resetForm();
+    this.form.reset();
   }
 
 }
