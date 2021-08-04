258ad41473054e6ab6321dd4d6320d9754bcfd89
santt3131 <santt31@gmail.com>
2021-08-04 19:47:17 +0200

    finish to upload with image

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 //importamos el express
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -18,6 +19,7 @@ mongoose.connect("mongodb+srv://santt31:Alaska123@cluster0.oreks.mongodb.net/myF
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -24,7 +24,8 @@ export class PostsService {
             return {
               title: post.title,
               content: post.content,
-              id: post._id
+              id: post._id,
+              imagePath: post.imagePath
             };
         });
       }))
@@ -50,12 +51,13 @@ export class PostsService {
     postData.append("content", content);
     postData.append("image", image, title);
 
-    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', postData)
+    this.http.post<{ message: string, post: Post }>('http://localhost:3000/api/posts', postData)
       .subscribe((responseData) => {
         const post:Post ={
-          id: responseData.postId,
+          id: responseData.post.id,
           title: title,
-          content: content
+          content: content,
+          imagePath: responseData.post.imagePath
         }
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
@@ -78,7 +80,12 @@ export class PostsService {
   }
 
   updatePost(id:string, title: string , content:string){
-    const post:Post = {id :id , title:title, content:content};
+    const post:Post = {
+      id :id ,
+      title:title,
+      content:content,
+      imagePath: null
+    };
     this.http.put("http://localhost:3000/api/posts/" + id, post)
     .subscribe(()=>{
       const updatedPosts: Post[]= [...this.posts];
