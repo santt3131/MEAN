0aa297edd563072ee9325d00c4f488e0b014fba5
santt3131 <santt31@gmail.com>
2021-08-19 20:19:46 +0200

    BACK-Elastic Beanstalk and FRONT-S3

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ mongoose.connect("mongodb+srv://santt31:" + process.env.MONGO_ATLAS_PW + "@clust
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://nodeangularpost-env-2.eba-jtaue27k.us-east-2.elasticbeanstalk.com/api"
 };
